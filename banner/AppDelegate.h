2c9ad52189bfa0882e41e6f8b8db49a7369e365e
pactera <pactera@pactera-2.local>
2017-02-22 15:12:11 +0800

    Initial Commit

---
+++ banner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  banner
+//
+//  Created by pactera on 17/2/22.
+//  Copyright © 2017年 pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ banner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  banner
+//
+//  Created by pactera on 17/2/22.
+//  Copyright © 2017年 pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
